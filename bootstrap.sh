b26626b86c4c9ebffbaca121b0d8feb69da0e383
sam@freighttrust.com <sam@freighttrust.com>
2020-06-30 10:16:01 -0700

    scripts

---
+++ bootstrap.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+git archive --remote=https://github.com/tianon/docker-brew-ubuntu-core/blob/dist-amd64/focal/ubuntu-focal-core-cloudimg-amd64-root.tar.gz
---
+++ curl.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+curl -O https://partner-images.canonical.com/core/focal/current/ubuntu-focal-core-cloudimg-amd64-root.tar.gz
